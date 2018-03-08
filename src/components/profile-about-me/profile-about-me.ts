9f308cfe148328dd79b6ef78e351e902e8ddaea9
Polous-Sergey <polous@ro.ru>
2018-03-08 14:07:18 +0200

    add about me page

--- src/components/profile-about-me/profile-about-me.ts
+++ src/components/profile-about-me/profile-about-me.ts
@@ -1,22 +1,86 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Camera, CameraOptions} from "@ionic-native/camera";
+import {NavController} from "ionic-angular";
+import {ChangePassPage} from "../../pages/change-pass/change-pass";
 
-/**
- * Generated class for the ProfileAboutMeComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
 @Component({
-  selector: 'profile-about-me',
-  templateUrl: 'profile-about-me.html'
+    selector: 'profile-about-me',
+    templateUrl: 'profile-about-me.html'
 })
 export class ProfileAboutMeComponent {
 
-  text: string;
+    city: string;
+    imageURI: any;
+    imageFileName: any;
 
-  constructor() {
-    console.log('Hello ProfileAboutMeComponent Component');
-    this.text = 'Hello World';
-  }
+    showList: boolean = false;
+    searchQuery: string = '';
+    items: string[];
+
+    constructor(public navCtrl: NavController,
+                private camera: Camera) {
+        this.initializeItems();
+        this.imageURI = 'assets/imgs/camera.png';
+    }
+
+    getImage() {
+        const options: CameraOptions = {
+            quality: 100,
+            destinationType: this.camera.DestinationType.FILE_URI,
+            sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+        };
+
+        this.camera.getPicture(options).then((imageData) => {
+            this.imageURI = imageData;
+        }, (err) => {
+            console.log(err);
+            // this.presentToast(err);
+        });
+    }
+
+    presentContactModal() {
+        this.navCtrl.push(ChangePassPage);
+
+    }
+
+    initializeItems() {
+        this.items = [
+            'Amsterdam',
+            'Berlin',
+            'Bueno Aires',
+            'Madrid',
+            'Paris'
+        ];
+    }
+
+    chooseItem(item) {
+        this.city = item;
+        this.showList = false;
+    }
+
+    getItems(ev: any) {
+        // Reset items back to all of the items
+        this.initializeItems();
+
+        // set val to the value of the searchbar
+        let val = ev.target.value;
+
+        // if the value is an empty string don't filter the items
+        if (val && val.trim() != '') {
+
+            // Filter the items
+            this.items = this.items.filter((item) => {
+                return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+            });
+
+            // Show the results
+            this.showList = true;
+        } else {
+
+            // hide the results when the query is empty
+            this.showList = false;
+        }
+    }
+
 
 }
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,96 +1,17 @@
 import {Component} from '@angular/core';
-import {LoadingController, ModalController, NavController, ToastController} from 'ionic-angular';
-import {InfoPage} from "../info/info";
-import {ChangePassPage} from "../change-pass/change-pass";
-import {AddSkillPage} from "../add-skill/add-skill";
-import {FileTransfer} from "@ionic-native/file-transfer";
-import {Camera, CameraOptions} from "@ionic-native/camera";
 
 @Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html'
+    selector: 'page-profile',
+    templateUrl: 'profile.html'
 })
 export class ProfilePage {
+
     segmentValue: string = 'about_me';
-  city: string;
-  imageURI:any;
-  imageFileName:any;
-
-  constructor(public navCtrl: NavController,
-              public modalCtrl: ModalController,
-              private transfer: FileTransfer,
-              private camera: Camera,
-              public loadingCtrl: LoadingController,
-              public toastCtrl: ToastController) {
-    this.initializeItems();
-    this.imageURI = 'assets/imgs/camera.png';
-
-  }
-
-  getImage() {
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
-      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
-    };
-
-    this.camera.getPicture(options).then((imageData) => {
-      this.imageURI = imageData;
-    }, (err) => {
-      console.log(err);
-      // this.presentToast(err);
-    });
-  }
-
-  presentContactModal() {
-    this.navCtrl.push(ChangePassPage);
-
-  }
-
-  showList: boolean = false;
-  searchQuery: string = '';
-  items: string[];
-
-  initializeItems() {
-    this.items = [
-      'Amsterdam',
-      'Berlin',
-      'Bueno Aires',
-      'Madrid',
-      'Paris'
-    ];
-  }
-
-  chooseItem(item) {
-    this.city = item;
-    this.showList = false;
-  }
-
-  getItems(ev: any) {
-    // Reset items back to all of the items
-    this.initializeItems();
-
-    // set val to the value of the searchbar
-    let val = ev.target.value;
-
-    // if the value is an empty string don't filter the items
-    if (val && val.trim() != '') {
-
-      // Filter the items
-      this.items = this.items.filter((item) => {
-        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
-      });
-
-      // Show the results
-      this.showList = true;
-    } else {
-
-      // hide the results when the query is empty
-      this.showList = false;
-    }
-  }
-
     editSkills: boolean = false;
+
+    constructor() {
+
+    }
 
     selectedAllSkills() {
 
@@ -99,6 +20,4 @@ export class ProfilePage {
     editSkillsFunc() {
         this.editSkills = !this.editSkills;
     }
-
-
 }
