fcee5f74ece2b4bdf754881ff9ebb259f949358f
Anton <tosha.gritzenko@meta.ua>
2018-03-13 14:16:10 +0200

    Merge branch 'master' of https://github.com/okaminouta/builders into anton
    # Conflicts:
    #	src/pages/login/login.html
    #	src/pages/profile/profile.html
    #	src/pages/profile/profile.scss

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -17,111 +17,108 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+    selector: 'page-login',
+    templateUrl: 'login.html',
 })
 export class LoginPage {
-  // loginForm: FormGroup;
+    // loginForm: FormGroup;
 
-  userData = {
-    phone: '+380',
-    password: null,
-  };
-validation: any;
-
-
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              public alertCtrl: AlertController,
-              public util: UtilityProvider,
-              public  user: UserProvider,
-              public formBuilder: FormBuilder) {
-    this.validation = this.util.validation;
-
-  }
-
-  loginForm = this.formBuilder.group({
-    phone: ['', Validators.compose([
-      Validators.maxLength(14),
-      Validators.minLength(14),
-      Validators.required])],
-    password: ['', Validators.compose([
-      Validators.maxLength(30),
-      Validators.minLength(6),
-      Validators.required])]
-  });
+    userData = {
+        phone: '+380',
+        password: null,
+    };
+    validation: any;
 
 
+    constructor(public navCtrl: NavController,
+                public navParams: NavParams,
+                public alertCtrl: AlertController,
+                public util: UtilityProvider,
+                public  user: UserProvider,
+                public formBuilder: FormBuilder) {
+        this.validation = this.util.validation;
+
+    }
+
+    loginForm = this.formBuilder.group({
+        phone: ['', Validators.compose([
+            Validators.maxLength(14),
+            Validators.minLength(14),
+            Validators.required])],
+        password: ['', Validators.compose([
+            Validators.maxLength(30),
+            Validators.minLength(6),
+            Validators.required])]
+    });
 
 
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad LoginPage');
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+    }
 
-  }
-
-  login () {
-    console.log(this.loginForm)
-    if (this.util.credentialsCheck(this.userData)) {
-      this.user.login(this.userData).then((res: any) => {
-        if (res) {
-          this.navCtrl.push(TabsPage);
+    login() {
+        console.log(this.loginForm)
+        if (this.util.credentialsCheck(this.userData)) {
+            this.user.login(this.userData).then((res: any) => {
+                if (res) {
+                    this.navCtrl.push(TabsPage);
+                }
+            });
         }
-      });
     }
-  }
 
 
-  updateInput() {
-    if (this.userData.phone == undefined || this.userData.phone == "") {
-      this.userData.phone = '+380()';
+    updateInput() {
+        if (this.userData.phone == undefined || this.userData.phone == "") {
+            this.userData.phone = '+380()';
+        }
     }
-  }
 
-  goToRegistration() {
-    console.log('Naw ctrl', this.navCtrl);
-    this.navCtrl.push(SignUpPage);
-  }
+    goToRegistration() {
+        console.log('Naw ctrl', this.navCtrl);
+        this.navCtrl.push(SignUpPage);
+    }
 
-  goToSlider() {
-    this.navCtrl.push(SliderPage);
-  }
+    goToSlider() {
+        this.navCtrl.push(SliderPage);
+    }
 
 /////////////
 
-  isActive = true;
+    isActive = true;
 
-  showPass() {
-    this.isActive = !this.isActive;
-  }
+    showPass() {
+        this.isActive = !this.isActive;
+    }
 
-  showPrompt() {
-    let prompt = this.alertCtrl.create({
-      title: 'Login',
-      message: '<h1>hel</h1>',
-      inputs: [
-        {
-          name: 'title',
-          placeholder: 'Title'
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: data => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Ok',
-          handler: data => {
-            console.log('Saved clicked');
-            this.navCtrl.push(TabsPage);
-          }
-        }
-      ]
-    });
-    prompt.present();
-  }
+    showPrompt() {
+        let prompt = this.alertCtrl.create({
+            title: 'Login',
+            message: '<h1>hel</h1>',
+            inputs: [
+                {
+                    name: 'title',
+                    placeholder: 'Title'
+                },
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    handler: data => {
+                        console.log('Cancel clicked');
+                    }
+                },
+                {
+                    text: 'Ok',
+                    handler: data => {
+                        console.log('Saved clicked');
+                        this.navCtrl.push(TabsPage);
+                    }
+                }
+            ]
+        });
+        prompt.present();
+    }
 
 }
--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -1,5 +1,5 @@
 import {Component, ElementRef, HostBinding} from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {LoginPage} from "../login/login";
 import {TabsPage} from "../tabs/tabs";
 import {UserProvider} from "../../providers/user/user";
@@ -13,51 +13,52 @@ import {UserProvider} from "../../providers/user/user";
 
 @IonicPage()
 @Component({
-  selector: 'page-sign-up',
-  templateUrl: 'sign-up.html',
+    selector: 'page-sign-up',
+    templateUrl: 'sign-up.html',
 })
 export class SignUpPage {
-  // @HostBinding('style.backgroundColor') backgroundColor: string;
-  userData = {
-    phone: '+380',
-    password: null,
-  };
+    // @HostBinding('style.backgroundColor') backgroundColor: string;
+    userData = {
+        phone: '+380',
+        password: null,
+    };
 
 
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              public userProvider: UserProvider) {
-    // sometimes falls here wtf
-    // this.userData.name = null;
-    // this.userData.pass = null;
-  }
-
-  ionViewDidLoad () {
-    console.log('ionViewDidLoad SignUpPage');
-  }
-
- register () {
-    this.userProvider.register(this.userData).then( (data: any) => {
-      console.log(data,'signupdata')
-    });
-    // console.log('test',test)
-
-    this.navCtrl.push(TabsPage);
- }
-
-  updateInput () {
-    if (this.userData.phone == undefined || this.userData.phone == "") {
-      this.userData.phone = '+380()';
+    constructor(public navCtrl: NavController,
+                public navParams: NavParams,
+                public userProvider: UserProvider) {
+        // sometimes falls here wtf
+        // this.userData.name = null;
+        // this.userData.pass = null;
     }
-  }
 
-  goToLogin () {
-    this.navCtrl.push(LoginPage);
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad SignUpPage');
+    }
 
-  isActive = true;
-  showPass () {
-    this.isActive = !this.isActive;
-  }
+    register() {
+        this.userProvider.register(this.userData).then((data: any) => {
+            console.log(data, 'signupdata')
+        });
+        // console.log('test',test)
+
+        this.navCtrl.push(TabsPage);
+    }
+
+    updateInput() {
+        if (this.userData.phone == undefined || this.userData.phone == "") {
+            this.userData.phone = '+380()';
+        }
+    }
+
+    goToLogin() {
+        this.navCtrl.push(LoginPage);
+    }
+
+    isActive = true;
+
+    showPass() {
+        this.isActive = !this.isActive;
+    }
 
 }
