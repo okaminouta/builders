26a63bd123b4bfc89a2063df5557837abf85a630
Anton <tosha.gritzenko@meta.ua>
2018-03-26 16:20:14 +0300

    fix some bugs

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -18,105 +18,112 @@ import {ProfilePage} from "../profile/profile";
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+    selector: 'page-login',
+    templateUrl: 'login.html',
 })
 export class LoginPage {
-  userData = {
-    phone: '+380 (__) __-__-___',
-    password: null
-  };
-  validation: any;
+    userData = {
+        phone: null,
+        password: null
+    };
+    validation: any;
 
 
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              public alertCtrl: AlertController,
-              public util: UtilityProvider,
-              public  user: UserProvider,
-              public formBuilder: FormBuilder) {
-   this.user.firstEnter().get().then((data)=>{
-      if(!data){
-        this.navCtrl.push(SliderPage);
-        this.user.firstEnter().setTrue();
-      }
-    });
-    this.validation = this.util.validation;
-  }
-
-  loginForm = this.formBuilder.group({
-    phone: ['', Validators.compose([
-      Validators.maxLength(19),
-      Validators.minLength(19),
-      Validators.required])],
-    password: ['', Validators.compose([
-      Validators.maxLength(30),
-      Validators.minLength(6),
-      Validators.required])]
-  });
-
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-
-  }
-
-  login() {
-    console.log(this.loginForm)
-    if (this.util.credentialsCheck(this.userData) && this.loginForm.valid) {
-      this.user.login(this.util.cut(this.userData)).then((res: any) => {
-        if (res) {
-            this.navCtrl.push(ProfilePage);
-        }
-      });
+    constructor(public navCtrl: NavController,
+                public navParams: NavParams,
+                public alertCtrl: AlertController,
+                public util: UtilityProvider,
+                public  user: UserProvider,
+                public formBuilder: FormBuilder) {
+        this.user.getUser().then((res)=> {
+            if(res){
+                this.navCtrl.push(TabsPage);
+            }
+        });
+        this.user.firstEnter().get().then((data)=>{
+            if(!data){
+                this.navCtrl.push(SliderPage);
+                this.user.firstEnter().setTrue();
+            }
+        });
+        this.validation = this.util.validation;
     }
-  }
 
-  goToRegistration() {
-    console.log('Naw ctrl', this.navCtrl);
-    this.navCtrl.push(SignUpPage);
-  }
+    // loginForm = this.formBuilder.group({
+    //   phone: ['', Validators.compose([
+    //     Validators.maxLength(14),
+    //     Validators.minLength(14),
+    //     Validators.required])],
+    //   password: ['', Validators.compose([
+    //     Validators.maxLength(30),
+    //     Validators.minLength(6),
+    //     Validators.required])]
+    // });
 
-  forgotPass () {
-    this.navCtrl.push(TabsPage);
-  }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad LoginPage');
+
+    }
+
+    login() {
+        // console.log(this.loginForm)
+        // && this.loginForm.valid
+
+        if (this.util.credentialsCheck(this.userData) ) {
+            this.user.login(this.userData).then((res: any) => {
+                if (res) {
+                    this.navCtrl.push(ProfilePage);
+                }
+            });
+        }
+    }
+
+    goToRegistration() {
+        console.log('Naw ctrl', this.navCtrl);
+        this.navCtrl.push(SignUpPage);
+    }
+
+    forgotPass () {
+        this.navCtrl.push(TabsPage);
+    }
 
 
 /////////////
 
-  isActive = true;
+    isActive = true;
 
-  showPass() {
-    this.isActive = !this.isActive;
-  }
+    showPass() {
+        this.isActive = !this.isActive;
+    }
 
-  showPrompt() {
-    let prompt = this.alertCtrl.create({
-      title: 'Login',
-      message: '<h1>hel</h1>',
-      inputs: [
-        {
-          name: 'title',
-          placeholder: 'Title'
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: data => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Ok',
-          handler: data => {
-            console.log('Saved clicked');
-            this.navCtrl.push(TabsPage);
-          }
-        }
-      ]
-    });
-    prompt.present();
-  }
+    showPrompt() {
+        let prompt = this.alertCtrl.create({
+            title: 'Login',
+            message: '<h1>hel</h1>',
+            inputs: [
+                {
+                    name: 'title',
+                    placeholder: 'Title'
+                },
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    handler: data => {
+                        console.log('Cancel clicked');
+                    }
+                },
+                {
+                    text: 'Ok',
+                    handler: data => {
+                        console.log('Saved clicked');
+                        this.navCtrl.push(TabsPage);
+                    }
+                }
+            ]
+        });
+        prompt.present();
+    }
 
 }
