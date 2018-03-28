12967ee243ec3d792182a767c806e21285f12457
Anton <tosha.gritzenko@meta.ua>
2018-03-28 14:54:16 +0300

    fix some bugs

--- src/components/profile-about-me/profile-about-me.ts
+++ src/components/profile-about-me/profile-about-me.ts
@@ -11,132 +11,132 @@ import {CameraOptionsPage} from "../../pages/camera-options/camera-options";
 
 
 @Component({
-  selector: 'profile-about-me',
-  templateUrl: 'profile-about-me.html'
+    selector: 'profile-about-me',
+    templateUrl: 'profile-about-me.html'
 })
 export class ProfileAboutMeComponent implements OnChanges {
-  @Input() editProfile;
-  segmentValue;
-  userData: any;
-  city: string;
-  imageURI: any;
-  phone;
+    @Input() editProfile;
+    segmentValue;
+    userData: any;
+    city: string;
+    imageURI: any;
+    phone;
 
-  // disableFields: boolean = true;
+    // disableFields: boolean = true;
 
-  showList: boolean = false;
-  searchQuery: string = '';
-  items: string[];
+    showList: boolean = false;
+    searchQuery: string = '';
+    items: string[];
 
-  constructor(public navCtrl: NavController,
-              public util: UtilityProvider,
-              public modalCtrl: ModalController,
-              private user: UserProvider,
-              private media: MediaProvider,) {
-    this.initializeItems();
-    this.leaveCheck();
-    this.imageURI = 'assets/imgs/camera.png';
-    this.user.getProfile().then(res => {
-      if (res) {
-        this.userData.first_name = res.profile.first_name;
-        this.userData.last_name = res.profile.last_name;
-        this.userData.email = res.profile.email;
-        this.userData.city = res.profile.city;
-        this.userData.passport_id = res.profile.passport_id;
-        this.phone = '+380 ' +
-          res.profile.phone.toString().substring(0, 2) +
-          ' ' + res.profile.phone.toString().substring(2, 4) +
-          ' ' + res.profile.phone.toString().substring(4, 6) +
-          ' ' + res.profile.phone.toString().substring(6);
-        this.imageURI = res.photo_path;
-      }
-    });
-
-  }
-
-  leaveCheck() {
-    if (this.userData.first_name === null || this.userData.last_name === null) {
-      this.util.toast('Заповніть дані профіля', 'alert')
-    }
-  }
-
-  photoAddControl() {
-    let modal = this.modalCtrl.create(CameraOptionsPage);
-    modal.onDidDismiss((data) => {
-      if (data === 'delete') {
+    constructor(public navCtrl: NavController,
+                public util: UtilityProvider,
+                public modalCtrl: ModalController,
+                private user: UserProvider,
+                private media: MediaProvider,) {
+        this.initializeItems();
+        this.leaveCheck();
         this.imageURI = 'assets/imgs/camera.png';
-      } else {
-        this.media.getMedia(data).then((res) => {
-          this.imageURI = res;
+        this.user.getProfile().then(res => {
+            if (res) {
+                this.userData.first_name = res.profile.first_name;
+                this.userData.last_name = res.profile.last_name;
+                this.userData.email = res.profile.email;
+                this.userData.city = res.profile.city;
+                this.userData.passport_id = res.profile.passport_id;
+                this.phone = '+380 ' +
+                    res.profile.phone.toString().substring(0, 2) +
+                    ' ' + res.profile.phone.toString().substring(2, 4) +
+                    ' ' + res.profile.phone.toString().substring(4, 6) +
+                    ' ' + res.profile.phone.toString().substring(6);
+                this.imageURI = res.photo_path;
+            }
         });
-      }
-    });
-    modal.present()
-  }
 
+    }
 
-  ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
-    if (!changes.editProfile.firstChange && !this.editProfile) {
-      console.log(this.userData)
-      this.user.setProfile(this.userData);
-      this.user.firstEnter().get().then((res) => {
-        if (res) {
-          this.util.toast('Заповніть ваші навички', 'alert');
-          this.util.changeTab('skills');
+    leaveCheck() {
+        if (this.userData.first_name === null || this.userData.last_name === null) {
+            this.util.toast('Заповніть дані профіля', 'alert')
         }
-      })
     }
-  }
 
-  presentContactModal() {
-    this.navCtrl.push(ChangePassPage);
-
-  }
-
-  initializeItems() {
-    this.items = [
-      'Amsterdam',
-      'Berlin',
-      'Bueno Aires',
-      'Madrid',
-      'Paris'
-    ];
-    this.userData = {
-      first_name: null,
-      last_name: null,
-      email: null,
-      city: null,
-      passport_id: false,
-      photo: this.imageURI || null
+    photoAddControl() {
+        let modal = this.modalCtrl.create(CameraOptionsPage);
+        modal.onDidDismiss((data) => {
+            if (data === 'delete') {
+                this.imageURI = 'assets/imgs/camera.png';
+            } else {
+                this.media.getMedia(data).then((res) => {
+                    this.imageURI = res;
+                });
+            }
+        });
+        modal.present()
     }
-  }
 
-  chooseItem(item) {
-    this.city = item;
-    this.showList = false;
-  }
 
-  getItems(ev: any) {
-    // Reset items back to all of the items
-    this.initializeItems();
-
-    // set val to the value of the searchbar
-    let val = ev.target.value;
-
-    // if the value is an empty string don't filter the items
-    if (val && val.trim() != '') {
-
-      // Filter the items
-      this.items = this.items.filter((item) => {
-        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
-      });
-
-      // Show the results
-      this.showList = true;
-    } else {
-
-      // hide the results when the query is empty
-      this.showList = false;
+    ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
+        if (!changes.editProfile.firstChange && !this.editProfile) {
+            console.log(this.userData)
+            this.user.setProfile(this.userData);
+            this.user.firstEnter().get().then((res) => {
+                if (res) {
+                    this.util.toast('Заповніть ваші навички', 'alert');
+                    this.util.changeTab('skills');
+                }
+            })
+        }
     }
-  }
+
+    presentContactModal() {
+        this.navCtrl.push(ChangePassPage);
+
+    }
+
+    initializeItems() {
+        this.items = [
+            'Amsterdam',
+            'Berlin',
+            'Bueno Aires',
+            'Madrid',
+            'Paris'
+        ];
+        this.userData = {
+            first_name: null,
+            last_name: null,
+            email: null,
+            city: null,
+            passport_id: false,
+            photo: this.imageURI || null
+        }
+    }
+
+    chooseItem(item) {
+        this.city = item;
+        this.showList = false;
+    }
+
+    getItems(ev: any) {
+        // Reset items back to all of the items
+        this.initializeItems();
+
+        // set val to the value of the searchbar
+        let val = ev.target.value;
+
+        // if the value is an empty string don't filter the items
+        if (val && val.trim() != '') {
+
+            // Filter the items
+            this.items = this.items.filter((item) => {
+                return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+            });
+
+            // Show the results
+            this.showList = true;
+        } else {
+
+            // hide the results when the query is empty
+            this.showList = false;
+        }
+    }
 }
--- src/providers/utility/utility.ts
+++ src/providers/utility/utility.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {EventEmitter, Injectable, Output} from '@angular/core';
-import { ToastController } from 'ionic-angular';
+import {ToastController} from 'ionic-angular';
 
 /*
   Generated class for the UtilityProvider provider.
@@ -10,94 +10,95 @@ import { ToastController } from 'ionic-angular';
 */
 @Injectable()
 export class UtilityProvider {
-  @Output() changeTabs = new EventEmitter<string>();
-  constructor(public http: HttpClient,
-              public toastCtrl: ToastController,) {
-    console.log('Hello UtilityProvider Provider');
-  }
+    @Output() changeTabs = new EventEmitter<string>();
 
-  changeTab (str){
-    this.changeTabs.emit(str);
-  }
-
-  toast (msg: string, cssClass: string, position: string = 'top' ) {
-    let toast = this.toastCtrl.create({
-      message: msg,
-      position: position,
-      duration: 3000,
-      cssClass: cssClass,
-      dismissOnPageChange: false
-    });
-    toast.present();
-  }
-
-  credentialsCheck(data: any) {
-
-    if (!data.phone || !data.password){
-      this.toast('Заповніть всі поля', 'alert');
-      return false;
+    constructor(public http: HttpClient,
+                public toastCtrl: ToastController,) {
+        console.log('Hello UtilityProvider Provider');
     }
-    if (data.phone.length != 9){
-      this.toast('Номер телефону має бути 12 цифр', 'alert');
-      return false;
+
+    changeTab(str) {
+        this.changeTabs.emit(str);
     }
-    if (data.password.length < 6 || data.password.length > 15 ){
-      this.toast('Пароль має бути 6-15 символів', 'alert');
-      return false;
+
+    toast(msg: string, cssClass: string, position: string = 'top') {
+        let toast = this.toastCtrl.create({
+            message: msg,
+            position: position,
+            duration: 3000,
+            cssClass: cssClass,
+            dismissOnPageChange: false
+        });
+        toast.present();
     }
-    return true;
-  }
+
+    credentialsCheck(data: any) {
+
+        if (!data.phone || !data.password) {
+            this.toast('Заповніть всі поля', 'alert');
+            return false;
+        }
+        if (data.phone.length != 9) {
+            this.toast('Номер телефону має бути 12 цифр', 'alert');
+            return false;
+        }
+        if (data.password.length < 6 || data.password.length > 15) {
+            this.toast('Пароль має бути 6-15 символів', 'alert');
+            return false;
+        }
+        return true;
+    }
 
 
-  cut(data: any) {
-    let cred = {
-      phone: null,
-      password: null
-    };
-    cred.phone = parseInt( data.phone.substr(3).replace(/[^0-9]/g, ''));
-    cred.password = data.password;
-    return cred;
-  }
-
-  validation (form:any) {
-    return {
-      phone: () => {
-        if (form.controls.phone.errors &&
-          form.controls.phone.errors.required &&
-          form.controls.phone.dirty) {
-          return 'Це поле є обов\'язковим!';
-        }
-        else if (form.controls.phone.dirty &&
-          form.controls.phone.errors &&
-          form.controls.phone.errors.minlength ||
-          form.controls.phone.dirty &&
-          form.controls.phone.errors &&
-          form.controls.phone.errors.maxlength) {
-          return 'Номер телефону має бути 12 цифер';
-        }
-        else {
-          return false
-        }
-      },
-      password: () => {
-        if (form.controls.password.errors &&
-          form.controls.password.errors.required &&
-          form.controls.password.dirty) {
-          return 'Це поле є обов\'язковим!';
-        }
-        else if (form.controls.password.dirty &&
-          form.controls.password.errors &&
-          form.controls.password.errors.minlength ||
-          form.controls.password.dirty &&
-          form.controls.password.errors &&
-          form.controls.password.errors.maxlength) {
-          return 'Пароль має бути 6-30 символів';
-        }
-        else {
-          return false
-        }
-      }
+    cut(data: any) {
+        let cred = {
+            phone: null,
+            password: null
+        };
+        cred.phone = parseInt(data.phone.substr(3).replace(/[^0-9]/g, ''));
+        cred.password = data.password;
+        return cred;
     }
-  }
+
+    validation(form: any) {
+        return {
+            phone: () => {
+                if (form.controls.phone.errors &&
+                    form.controls.phone.errors.required &&
+                    form.controls.phone.dirty) {
+                    return 'Це поле є обов\'язковим!';
+                }
+                else if (form.controls.phone.dirty &&
+                    form.controls.phone.errors &&
+                    form.controls.phone.errors.minlength ||
+                    form.controls.phone.dirty &&
+                    form.controls.phone.errors &&
+                    form.controls.phone.errors.maxlength) {
+                    return 'Номер телефону має бути 12 цифер';
+                }
+                else {
+                    return false
+                }
+            },
+            password: () => {
+                if (form.controls.password.errors &&
+                    form.controls.password.errors.required &&
+                    form.controls.password.dirty) {
+                    return 'Це поле є обов\'язковим!';
+                }
+                else if (form.controls.password.dirty &&
+                    form.controls.password.errors &&
+                    form.controls.password.errors.minlength ||
+                    form.controls.password.dirty &&
+                    form.controls.password.errors &&
+                    form.controls.password.errors.maxlength) {
+                    return 'Пароль має бути 6-30 символів';
+                }
+                else {
+                    return false
+                }
+            }
+        }
+    }
 
 }
