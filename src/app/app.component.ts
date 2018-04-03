4fd32c1b0240cfbf5ef66771f9966e853b641ef7
Anton <tosha.gritzenko@meta.ua>
2018-04-03 18:16:47 +0300

    only (app.components)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,43 +9,45 @@ import {UserProvider} from "../providers/user/user";
 import {SliderPage} from "../pages/slider/slider";
 import {TabsPage} from "../pages/tabs/tabs";
 @Component({
-  templateUrl: 'app.html',
-  providers: [
-    ScreenOrientation
-  ]
+    templateUrl: 'app.html',
+    providers: [
+        ScreenOrientation
+    ]
 })
 export class MyApp {
-  @ViewChild(Nav) navCtrl: Nav;
-  rootPage: any;
+    @ViewChild(Nav) navCtrl: Nav;
+    rootPage: any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
-              public  user: UserProvider,
-               private screenOrientation: ScreenOrientation) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      if (platform.is('cordova')) {
-        this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
-      }
-      statusBar.styleDefault();
-      this.user.firstEnter().get().then((res)=> {
-        if(!res){
-          this.user.firstEnter().setTrue();
-          this.navCtrl.setRoot(SliderPage)
-        }
-        if (res === 'Finished'){
-          this.user.getUser().then((res)=> {
-            if(res){
-              this.navCtrl.setRoot(TabsPage)
-            } else {
-              this.navCtrl.setRoot(LoginPage);
+    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+                public  user: UserProvider,
+                private screenOrientation: ScreenOrientation) {
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            if (platform.is('cordova')) {
+                this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
             }
-          })
-        }
-      });
-      splashScreen.hide();
-    });
-  }
+            statusBar.styleDefault();
+            this.user.firstEnter().get().then((res)=> {
+                if(!res){
+                    this.user.firstEnter().setTrue();
+                    this.navCtrl.setRoot(SliderPage)
+                }
+                if (res === 'Finished'){
+                    this.user.getUser().then((res)=> {
+                        if(res){
+                            this.navCtrl.setRoot(TabsPage)
+                        } else {
+                            this.navCtrl.setRoot(LoginPage);
+                        }
+                    })
+                }else {
+                    this.navCtrl.setRoot(LoginPage);
+                }
+            });
+            splashScreen.hide();
+        });
+    }
 
 
 }
